beb2bd06fbbe9343b299261d5c28af068bf4edfd
Olga Królikowska <olga.szydlowska2@gmail.com>
2023-03-30 21:33:17 +0200

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Olga Królikowska on 30/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
