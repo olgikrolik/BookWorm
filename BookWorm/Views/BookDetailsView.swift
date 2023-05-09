2f28354c1c8339203a78f2b930722d68feb98bc4
Olga Królikowska <olga.szydlowska2@gmail.com>
2023-05-09 09:07:07 +0200

    Edit text on the second screen

--- BookWorm/Views/BookDetailsView.swift
+++ BookWorm/Views/BookDetailsView.swift
@@ -27,19 +27,36 @@ struct BookDetailsView: View {
                         .resizable()
                         .aspectRatio(contentMode: .fill)
                         .frame(width: 150, height: 230)
+                        .padding(.top, 50)
                 } placeholder: {
                     ProgressView()
                 }
                 Text("IT STARTS WITH US")
                     .foregroundColor(.accentColor)
                     .font(.custom("Montserrat-Regular", size: 20))
-                VStack {
-                        
+                    .padding(.top, 20)
+                VStack (alignment: .leading) {
+                    Text(mergeDefinitionAndDescription(definition: "Author: ", description: "Coleen Hoover"))
+                    Text(mergeDefinitionAndDescription(definition: "ISBN: ", description: "9781668001233, 1668001233"))
+                    Text(mergeDefinitionAndDescription(definition: "Page count: ", description: "336"))
+                    Text(mergeDefinitionAndDescription(definition: "Published date: ", description: "18 October 2022"))
                 }
-                
+                .frame(maxWidth: 300)
+                .padding(.top, 5)
+                Spacer()
             }
         }
     }
+    func mergeDefinitionAndDescription(definition: String, description: String) -> AttributedString {
+        var definitionAttributedString = AttributedString(definition)
+        definitionAttributedString.font = .custom("Montserrat-Regular", size: 14)
+        
+        var descriptionAttributedString = AttributedString(description)
+        descriptionAttributedString.font = .custom("Montserrat-ExtraLight", size: 14)
+        
+        return definitionAttributedString + descriptionAttributedString
+    }
+    
 }
 
 struct BookDetailsView_Previews: PreviewProvider {
